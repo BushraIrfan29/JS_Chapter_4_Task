c25fc66c69a331a553dff711612b82ac4d5963e9
BushraIrfan29 <bushrairfan2103@gmail.com>
2024-08-12 18:05:33 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,35 @@
+//QUESTION 1
+var name;
+var age;
+var roll_no;
+
+//QUESTION 2
+//Legal Variables
+var firstName;
+var age;
+var _score;
+var totalAmount;
+var isValid;
+
+//Illegal Variables
+//var 2ndPlace;
+//var !class;
+//var total-amount;
+//var @value;
+//var %function;
+
+//Question 3
+var JS_Rules= "Rules for naming JS variables" + "<br/>" + "<br/>";
+document.write(JS_Rules);
+
+var var_rule="Variable names can only contain  _ , $ and number For example: $my_firstVariable" +"<br/>" +"<br/>";
+document.write(var_rule);
+
+var var_begin="Variables must begin with a letter, _ or $. For example $name, _name or name" +"<br/>" +"<br/>";
+document.write(var_begin);
+
+var var_case= "Variable names are case sensitive ." + "<br/>" + "<br/>";
+document.write(var_case);
+
+var var_name="Variable names should not be JS keywords." + "<br/>" + "<br/>";
+document.write(var_name);
